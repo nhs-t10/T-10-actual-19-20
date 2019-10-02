952a8d8456b6b2097de971160640d3a9efac97d3
alko08 <alexk@thekoppels.net>
2019-10-02 08:06:04 -0400

    Drive Code on Robot
    Drive code that worked!

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncodersTest.java
@@ -0,0 +1,24 @@
+//package org.firstinspires.ftc.teamcode;
+//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+//
+//@TeleOp(name = "EnBROders")
+//
+//public class EncodersTest extends Library
+//{
+//    boolean yeet = true;
+//    public void init() {
+//        //initialize_robot();
+//    }
+//    public void loop()
+//    {
+//        yayeet();
+//    }
+//    public void yayeet()
+//    {
+//        if(yeet)
+//        {
+//            driveUntil(100);
+//            yeet=false;
+//        }
+//    }
+//}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MimingWriter.java
@@ -0,0 +1,43 @@
+//package org.firstinspires.ftc.teamcode;
+//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+//
+//import java.io.FileWriter;
+//import java.io.IOException;
+//
+//@TeleOp(name = "MimingWriter")
+//public class MimingWriter extends Library
+//{
+//    public void init()
+//    {
+//    }
+//
+//    public void loop()
+//    {
+//        float curLimit = 2;
+//        float linear = gamepad1.left_stick_y;
+//        float side = gamepad1.left_stick_x;
+//        float rotation = gamepad1.right_stick_x;
+//
+//        if(gamepad1.right_stick_button)
+//        {
+//            curLimit -= .5;
+//
+//            if (curLimit < 1)
+//                curLimit = 2;
+//        }
+//
+//        omni(linear/curLimit, side/curLimit, rotation/curLimit);
+//
+//        try
+//        {
+//            FileWriter writer = new FileWriter("MimingFile.txt", true);
+//            writer.write(linear/curLimit + " " + side/curLimit + " " + rotation/curLimit);
+//            writer.close();
+//        }
+//
+//        catch (IOException ioe)
+//        {
+//            ioe.printStackTrace();
+//        }
+//    }
+//}
