d13f9812fbaacd43acea7ec4103c12e8fac7a5df
TheGreatRambler <aehart684351@gmail.com>
2019-10-03 15:51:06 -0400

    Im cool

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cvBase.java
@@ -0,0 +1,13 @@
+public class cvBase {
+	private enum trackingStates {
+		SKY_STONE
+	}
+
+	cvBase() {}
+
+	private findBounds(trackingStates objectToTrack) {
+		if (objectToTrack == trackingStates.SKY_STONE) {
+			
+		}
+	}
+}
