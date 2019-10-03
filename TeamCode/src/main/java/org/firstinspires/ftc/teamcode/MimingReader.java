3f0b5c5175be3f7613b4d36bb018044ca36c0f1a
JoeyTM1313 <42985043+JoeyTM1313@users.noreply.github.com>
2019-10-03 15:29:11 -0400

    Update
    Simon T's Update

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MimingReader.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+@Autonomous(name = "MimingReader")
+public class MimingReader extends Library
+{
+    public void init()
+    {
+        FileReader reader = new FileReader("MimingFile.txt");
+        BufferedReader bufferedReader = new BufferedReader(reader);
+    }
+
+    public void loop()
+    {
+        float[] values = new float[3];
+
+        try
+        {
+            String line = bufferedReader.nextLine();
+
+            if(line != null)
+            {
+                int first = IndexOf(" ");
+                int second = line.indexOf(" ", first);
+
+                values[0] = Float.parseFloat(line.substring(0, first));
+                values[0] = Float.parseFloat(line.substring(first + 1, second));
+                values[0] = Float.parseFloat(line.substring(second + 1));
+            }
+
+            else
+            {
+                reader.close();
+            }
+        }
+
+        catch (IOException ioe)
+        {
+            ioe.printStackTrace();
+        }
+
+        omni(values[0], values[1], values[2]);
+    }
+}
