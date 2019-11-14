b4732da758e5f8883832f927ac81874717e7096a
SimonInNeedham <33469288+SimonInNeedham@users.noreply.github.com>
2019-11-14 15:36:44 -0500

    Changes to Miming

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MimingWriter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MimingWriter.java
@@ -8,21 +8,11 @@ import java.io.File;
 @TeleOp(name = "MimingWriter")
 public class MimingWriter extends Library
 {
+    BufferedWriter writer = null;
+
     public void init()
     {
         hardwareInit();
-    }
-
-    /* The following loop accesses the "linear," "side," and
-    "rotation" values from gamepad1, uses them as paramaters
-    for the drive function (curlimit ensures that the values
-    gradually increase), and records them in MimingFile.txt
-    (to be later accessed by "MimingReader.java"). */
-
-    public void loop()
-    {
-        float linear, side, rotation;
-        BufferedWriter writer = null;
 
         try
         {
@@ -35,29 +25,32 @@ public class MimingWriter extends Library
         {
             ioe.printStackTrace();
         }
+    }
 
-        for (int i = 0; i < 10000; i++)
+    /* The following loop accesses the "linear," "side," and
+    "rotation" values from gamepad1, uses them as paramaters
+    for the drive function (curlimit ensures that the values
+    gradually increase), and records them in MimingFile.txt
+    (to be later accessed by "MimingReader.java"). */
+
+    public void loop()
+    {
+        float linear = gamepad1.left_stick_y;
+        float side = gamepad1.left_stick_x;
+        float rotation = gamepad1.right_stick_x;
+
+        drive(linear, side, rotation, 0);
+        telemetry.addData("Values: ", linear + "\n " + rotation + "\n " + side);
+
+        try
         {
-            linear = gamepad1.left_stick_y;
-            side = gamepad1.left_stick_x;
-            rotation = gamepad1.right_stick_x;
+            writer.write(linear + " " + rotation + " " + side);
+            writer.newLine();
+        }
 
-            drive(linear, side, rotation, 0);
-            telemetry.addData("Values: ", linear + "\n " + rotation + "\n " + side);
-
-            try
-            {
-                writer.write(linear + " " + rotation + " " + side);
-                writer.newLine();
-            }
-
-            catch(java.io.IOException ioe)
-            {
-                ioe.printStackTrace();
-            }
-
-            try { Thread.sleep(10); }
-            catch (InterruptedException ie) { ie.printStackTrace(); }
+        catch(java.io.IOException ioe)
+        {
+            ioe.printStackTrace();
         }
     }
 }
