3f73c47d20a1b8ed272cfa2293a283bda79d3534
SimonInNeedham <33469288+SimonInNeedham@users.noreply.github.com>
2019-11-19 19:21:11 -0500

    Made Cool Stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueBlock.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import java.io.*;
+
+@Autonomous(name = "BlueBlock")
+public class BlueBlock extends Library
+{
+    BufferedReader bufferedReader = null;
+
+    public void init()
+    {
+        hardwareInit();
+
+        try
+        {
+            File MimingFile = new File("/storage/emulated/0/FIRST/BlueBlock.txt");
+            bufferedReader = new BufferedReader(new FileReader(MimingFile));
+        }
+
+        catch (Exception ioe)
+        {
+            ioe.printStackTrace();
+        }
+    }
+
+    /* The following loop reads MimingFile.txt line by line,
+    accessing the "linear," "side," and "rotation" values and
+    using them as parameters for the "drive" function. */
+
+    public void loop()
+    {
+        String line = null;
+        float linear, side, rotation;
+
+        try
+        {
+            line = bufferedReader.readLine();
+        }
+
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        /* The object, "reader," is a FileReader that accesses
+        MimingFile.txt, which contains instructions for this autonomous'
+        actions. The object, "bufferReader," is a BufferedReader that
+        will be used to read the aforementioned txt file. */
+
+        int first = line.indexOf(" ");
+        int second = line.indexOf(" ", first + 1);
+
+        linear = Float.parseFloat(line.substring(0, first));
+        side = Float.parseFloat(line.substring(first + 1, second));
+        rotation = Float.parseFloat(line.substring(second + 1));
+
+        drive(linear, side, rotation, 0);
+    }
+}
