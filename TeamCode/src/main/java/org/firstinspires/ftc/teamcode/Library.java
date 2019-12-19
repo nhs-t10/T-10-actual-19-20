2210306be83bec583e0c1b7b1180c8da1b6e0673
alko08 <alexk@thekoppels.net>
2019-12-18 19:22:39 -0500

    back to old

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Library.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Library.java
@@ -153,39 +153,31 @@ public abstract class Library extends OpMode
         */
     }
 
-    public static float getStartPos(){
-        return backLeft.getCurrentPosition();
-    }
-
     //drive method for auto using encoders
     //float scalar to chose direction+power
     //float distance in CM is the magnitude of the distance traveled forwards or backwards
     //use this method if and only if no other sensors can be used to complete the motion
-
-
-    public static boolean driveForEncoders(float distanceInCM, float scalar, float startPosition)
+    public static void driveForEncoders(float distanceInCM, float scalar)
     {
-        if (Math.abs(backLeft.getCurrentPosition()) < (distanceInCM / 31.9f) * 1120f + startPosition)//31.9 is scuffed
+        float startPosition = backLeft.getCurrentPosition();
+        while (Math.abs(backLeft.getCurrentPosition()) < (distanceInCM / 31.9f) * 1120f + startPosition)//31.9 is scuffed
         {
             drive(scalar, 0, 0);
-            return true;
-        }else{
-            return false;
         }
+        drive(0, 0, 0);
     }
     //drive method for auto using encoders
     //float scalar to chose direction+power
     //float distance in CM is the magnitude of the distance traveled left or right
     //use this method if and only if no other sensors can be used to complete the motion
-    public static boolean slideForEncoders(float distanceInCM, float scalar, float startPosition)
+    public static void slideForEncoders(float distanceInCM, float scalar)
     {
-        if (Math.abs(backLeft.getCurrentPosition()) < (distanceInCM / 31.9f) * 1120f * TRACTION_SCALER + startPosition)
+        float startPosition = backLeft.getCurrentPosition();
+        while (Math.abs(backLeft.getCurrentPosition()) < (distanceInCM / 31.9f) * 1120f * TRACTION_SCALER + startPosition)
         {
             drive(0, 0, scalar);
-            return true;
-        }else{
-            return false;
         }
+        drive(0, 0, 0);
     }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedPlatformAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedPlatformAuto.java
@@ -11,10 +11,6 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public class RedPlatformAuto extends Library {
     public final int platformDistance = 60;
     public final float driveSpeed = .75f;
-    public int step;
-    public boolean startPosSet;
-    public float startPos;
-    public boolean drive;
 
     enum State{
         PLATFORM, PARKING, END
@@ -26,27 +22,11 @@ public class RedPlatformAuto extends Library {
         hardwareInit();
         backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);// we may use more motor encoders but some of the encoders have weird values
         state = State.PLATFORM;
-        step = 1;
-        startPosSet = false;
-        drive = true;
     }
     public void loop()
     {
         if(state == State.PLATFORM)
         {
-            if(step == 1 && startPosSet == false){
-                startPos = getStartPos();
-            }
-            if(step == 1 && startPosSet == true){
-                if(drive) {
-                    drive = slideForEncoders(60f, 0.5f, startPos);
-                }else{
-                    step = 2;
-                }
-            }
-            if(step == 2){
-                telemetry.addData("Step 2 woot", true);
-            }
 //            slideForEncoders(60, -100);
 //            driveForEncoders(120,-100);
             //grip(true);
@@ -57,14 +37,16 @@ public class RedPlatformAuto extends Library {
 //            drive(0,0,0);
 //            grip(false);
 
-            
+            state = State.PARKING;
         }
 
         if(state == State.PARKING)
         {
 //            slideForEncoders(150, 100);
-            //driveUntil(ColorSensor,0,0,DRIVE_SPEED);  //commented because I need to define color sensor
-            //drive(0,0,0);
+            while(!front1.isPressed()||!front2.isPressed()){
+                drive(-1f,0,0);//drives until touching wall
+            }
+            drive(0,0,0);
             state = State.END;
         }
     }
