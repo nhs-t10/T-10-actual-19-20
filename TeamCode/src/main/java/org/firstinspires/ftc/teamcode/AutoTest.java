e1ff2f0777a6e226c3d78cf7e2210131571c13df
Alex Malakov <32112772+AlexMHS@users.noreply.github.com>
2020-01-01 12:11:24 -0500

    auto test spits out encoder values
    Description

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
@@ -21,7 +21,7 @@ public class AutoTest extends Library {
 
     @Override public void init() {
         hardwareInit();
-        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);// we may use more motor encoders but some of the encoders have weird values
+        encodersInit();
 
     }
     public void loop()
@@ -34,6 +34,10 @@ public class AutoTest extends Library {
         boolean b = gamepad1.b;
         boolean liftUp = gamepad1.right_bumper;
         boolean liftDown = gamepad1.left_bumper;
+        telemetry.addData("front left:",frontLeft.getCurrentPosition());
+        telemetry.addData("front right:",frontRight.getCurrentPosition());
+        telemetry.addData("back left:",backLeft.getCurrentPosition());
+        telemetry.addData("back right:",backRight.getCurrentPosition());
         //float sums[] = drive(linear,rotation,side); //THIS ERRORS
 
         if(a)
