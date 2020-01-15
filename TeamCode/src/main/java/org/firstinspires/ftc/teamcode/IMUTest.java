de120980e54a4689afe5e6a464dac323e6d3b348
leoRysing <leshaigor@gmail.com>
2020-01-14 19:28:45 -0500

    Continued work on the test file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMUTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMUTest.java
@@ -1,8 +1,20 @@
 package org.firstinspires.ftc.teamcode;
 
-public class IMUTest {
-    public void loop(){
+public class IMUTest extends Library{
+    double xAccel = 0;
+    public void init()
+    {
+        hardwareInit();
+    }
+
+
+    public void loop() {
         //stuff lol
-        xAccel = imu.getXAcceleration();
+        if (gamepad1.a) {
+            turner.setDestination(90);
+            turner.updateDrive(imu);
+            xAccel = imu.getXAcceleration();
+            telemetry.addData("Current Horizontal Acceleration: ", xAccel);
+        }
     }
 }
