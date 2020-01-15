26001447a48e3f789d4d5e13b48edacb0de367a3
uniquenamesarehard <31969760+uniquenamesarehard@users.noreply.github.com>
2020-01-14 19:54:54 -0500

    added Auto Selection

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoSelection.java
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import java.util.*;
+
+@Autonomous(name = "Auto Selections")
+public class AutoSelection extends Library{
+    public enum Auto {STONE, FOUNDATION, PARKING}
+
+    public void init() {
+        hardwareInit();
+        Auto[] autos = Auto.values();
+        int stones = 0;
+        boolean foundation = false;
+        boolean parking = false;
+        boolean up = gamepad1.dpad_up;
+        boolean down = gamepad1.dpad_down;
+        boolean right = gamepad1.dpad_right;
+        boolean left = gamepad1.dpad_left;
+        Scanner f = new Scanner(System.in);
+        for (Auto types : autos){
+            switch (types) {
+                case STONE:
+                    telemetry.addLine("up for 0 Stone");
+                    telemetry.addLine("right for 1 Stone");
+                    telemetry.addLine("down for 2 Stone");
+                    telemetry.addLine("left for 3 Stone");
+
+                    if (up) { //change to up arrow for robot
+                        stones = 0;
+                    }
+
+                    else if (right) { //change to right arrow
+                        stones = 1;
+                    }
+
+                    else if (down) { //change to down arrow
+                        stones = 2;
+                    }
+
+                    else if (left) { //change to left arrow
+                        stones = 3;
+                    }
+                    break;
+                case FOUNDATION:
+                    telemetry.addLine("up for yes foundation");
+                    telemetry.addLine("down for no foundation");
+
+                    if (up) { //change to up arrow
+                        foundation = true;
+                    }
+                    else if (down){ //change to down arrow
+                        foundation = false;
+                    }
+                    break;
+                case PARKING:
+                    telemetry.addLine("up for yes parking");
+                    telemetry.addLine("down for no parking");
+
+                    if (up) { //change to up arrow
+                        parking = true;
+                    }
+                    else if (down) { //change to down arrow
+                        parking = false;
+                    }
+                    break;
+            }
+        }
+        telemetry.addLine(stones + " Stones"); //change to telemetry
+        telemetry.addLine(foundation + " : will do foundation");
+        telemetry.addLine(parking + " : will do parking");
+    }
+    public void loop(){}
+}
