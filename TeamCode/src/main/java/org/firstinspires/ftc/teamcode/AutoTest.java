a1290318a7d4174f22319ea8b0b9bdece1fb5447
Alex Malakov <32112772+AlexMHS@users.noreply.github.com>
2020-01-16 15:53:46 -0500

    easy and clean drive for
    it still needs testing so dont go to hard

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
@@ -1,83 +1,111 @@
-//package org.firstinspires.ftc.teamcode;
-//
-//import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-//import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-//import com.qualcomm.robotcore.hardware.DcMotor;
-//
-//
-//@TeleOp(name="encoders testing")//do not delete this test class used by sasha
-//public class AutoTest extends Library {
-//    public final int DRIVE_TO_PLATFORM = 100;
-//    public final float DRIVE_SPEED = .8f;
-//
-//    enum State
-//    {
-//        Pulling, Parking, Waiting
-//    }
-//
-//    State stat = State.Pulling;
-//
-//    @Override public void init() {
-//        hardwareInit();
-//        encodersInit();
-//
-//    }
-//    public void loop()
-//    {
-//
-//        float linear = gamepad1.left_stick_y;
-//        float side = gamepad1.left_stick_x;
-//        float rotation = gamepad1.right_stick_x;
-//        boolean a = gamepad1.a;
-//        boolean b = gamepad1.b;
-//        boolean liftUp = gamepad1.right_bumper;
-//        boolean liftDown = gamepad1.left_bumper;
-//        telemetry.addData("front left:",frontLeft.getCurrentPosition());
-//        telemetry.addData("front right:",frontRight.getCurrentPosition());
-//        telemetry.addData("back left:",backLeft.getCurrentPosition());
-//        telemetry.addData("back right:",backRight.getCurrentPosition());
-//        //float sums[] = drive(linear,rotation,side); //THIS ERRORS
-//
-//        if(a)
-//        {
-//            telemetry.addData("button getting pressed", "a is pressed");
-//            driveForEncoders(100,1);
-//        }
-//        if (b)
-//        {
-//            telemetry.addData("button getting pressed", "b is pressed");
-//
-//            driveForEncoders(150,.5f);
-//        }
-//        if(liftUp)
-//        {
-//            telemetry.addData("button getting pressed", "r1 is pressed");
-//
-//            strafeForEncoders(100,false);
-//        }
-//        if(liftDown)
-//        {
-//            telemetry.addData("button getting pressed", "a is pressed");
-//
-//            strafeForEncoders(150,false);
-//        }
-//
-//
-////        if(stat==State.Pulling)
-////        {
-////            driveForEncoders(DRIVE_TO_PLATFORM,-DRIVE_SPEED);
-////
-////            driveForEncoders(DRIVE_TO_PLATFORM,DRIVE_SPEED);
-////            stat=State.Parking;
-////        }
-////
-////        if(stat==State.Parking)
-////        {
-////            //driveUntil(ColorSensor,0,0,DRIVE_SPEED);  //commented because I need to define color sensor
-////            drive(0,0,0);
-////            stat=State.Waiting;
-////        }
-//    }
-//}
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp(name="encoders testing")//do not delete this test class used by sasha
+public class AutoTest extends Library {
+    public final int DRIVE_TO_PLATFORM = 100;
+    public final float DRIVE_SPEED = .8f;
+
+    EncodersMethod encoderObject = new EncodersMethod();
+
+    enum State
+    {
+        VIBIN, ROLLIN, ZOOMIN;
+        private static State[] vals = values();
+        public State next()
+        {
+            return vals[(this.ordinal()+1) % vals.length];
+        }
+    }
+
+    State stat = State.ROLLIN;
+
+    @Override public void init() {
+        hardwareInit();
+        encodersInit();
+
+    }
+
+
+
+
+    public void loop()
+    {
+
+        float linear = gamepad1.left_stick_y;
+        float side = gamepad1.left_stick_x;
+        float rotation = gamepad1.right_stick_x;
+        boolean a = gamepad1.a;
+        boolean b = gamepad1.b;
+        boolean liftUp = gamepad1.right_bumper;
+        boolean liftDown = gamepad1.left_bumper;
+        telemetry.addData("front left:",frontLeft.getCurrentPosition());
+        telemetry.addData("front right:",frontRight.getCurrentPosition());
+        telemetry.addData("back left:",backLeft.getCurrentPosition());
+        telemetry.addData("back right:",backRight.getCurrentPosition());
+
+        if(stat == State.ROLLIN&&encoderObject.driveCondition(stat)){
+            encoderObject.setGoalAndStart(getEncoderValue(),100,1);
+        }
+        if(stat == State.ZOOMIN&&encoderObject.driveCondition(stat))
+        {
+            encoderObject.setGoalAndStart(getEncoderValue(),-100,2);
+
+        }
+
+
+
+    }
+
+    private class EncodersMethod
+    {
+        float startPos;
+        float goal;
+        float scalar;
+        int receipt;
+        public EncodersMethod()
+        {
+            startPos=0;
+            goal=1000000000;
+            receipt =0;
+        }
+        public void slowDown()//this will do more later
+        {
+            scalar=1;
+        }
+        public boolean driveCondition(State stateInput)
+        {
+
+            slowDown();
+            if(Math.abs(goal)<Math.abs(getEncoderValue()-startPos))
+            {
+                drive(0,0,0);
+                goal+=10000000;//temp addition to allow the first iteration to happen
+                stateInput.next();
+                return true;
+            }else
+            {
+                drive(scalar,0,0);
+                return false;
+            }
+
+        }
+
+
+        public void setGoalAndStart(float startPos, float goal,int recieptCheck){
+            if( receipt !=recieptCheck)
+            {
+                this.startPos=startPos;
+                this.goal=goal;
+            }
+
+        }
+
+        public float convertToTicks(float distanceInCm)//this will do stuff soon
+        {
+            return 0;
+        }
+    }
+}
