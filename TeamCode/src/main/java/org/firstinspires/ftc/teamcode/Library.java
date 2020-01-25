1b73fa8ec6a9c581735481b6a0b08a280f50d607
uniquenamesarehard <31969760+uniquenamesarehard@users.noreply.github.com>
2020-01-25 14:27:20 -0500

    rotateFor was incorrect

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Library.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Library.java
@@ -472,7 +472,7 @@ public abstract class Library extends OpMode{
     public static void rotateFor(float degreesInRadians)
     {
         float start = getEncoderValue();
-        if(degreesInRadians>getEncoderValue()-getEncoderValue())
+        if(degreesInRadians>getEncoderValue()-start)
         {
             drive(0,(Math.abs(degreesInRadians))/(degreesInRadians),0);
         }
