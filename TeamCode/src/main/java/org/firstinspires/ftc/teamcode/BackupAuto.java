ae21a6a9b44a6bdc13b8a2a56c2ad6eb15b5ad6e
SimonInNeedham <33469288+SimonInNeedham@users.noreply.github.com>
2020-02-01 11:02:23 -0500

    Stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BackupAuto.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "Backup Auto")
+public class BackupAuto extends Library
+{
+    
+    private static final double ticksPerInch = 194.13;
+
+    public void init()
+    {
+        hardwareInit();
+    }
+
+    public void loop()
+    {
+
+    }
+}
