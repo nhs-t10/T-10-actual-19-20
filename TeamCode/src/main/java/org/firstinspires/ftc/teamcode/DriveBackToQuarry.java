9e3deb3a2151e9c1eec44501b1621dc3d3129976
SimonInNeedham <33469288+SimonInNeedham@users.noreply.github.com>
2020-02-11 18:20:29 -0500

    Stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveBackToQuarry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveBackToQuarry.java
@@ -1,18 +1,18 @@
 package org.firstinspires.ftc.teamcode;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class DriveBackToQuarry
 {
-    boolean started;
-    ElapsedTime clock;
+    float startEncoderValue;
+    final double ticksPerRotation = 194.13;
 
     public DriveBackToQuarry()
     {
-        started = false;
-        clock = new ElapsedTime();
+        startEncoderValue = Library.getEncoderValue();
     }
 
     public void driveToQuarry(boolean isOnBlueSide)
     {
+        if (Library.getEncoderValue() + 100 * ticksPerRotation > startEncoderValue)
+            Library.drive(.5f, 0, 0);
     }
 }
