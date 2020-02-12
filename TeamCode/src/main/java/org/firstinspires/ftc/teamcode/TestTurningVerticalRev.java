f370af8e69a801522e9a6d6d3047f1c72f41e4b7
mcheifetz <33294677+mcheifetz@users.noreply.github.com>
2020-02-11 19:21:29 -0500

    New Turning for vertical rev hub

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTurningVerticalRev.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "Turning Test")
+public class TestTurningVerticalRev extends Library {
+    TurningVerticalRev turner;
+
+    public void init(){
+        hardwareInit();
+        turner = new TurningVerticalRev();
+        turner.initImu(hardwareMap);
+    }
+
+    public void loop(){
+        double[] array = turner.turnDegrees(180);
+        telemetry.addData("DEST ANGLE: ", array[0]);
+        telemetry.addData("CLOCK: ", array[1]);
+        telemetry.addData("CURRENT ANGLE (2nd): ", array[2]);
+        telemetry.addData("ERROR: ", array[3]);
+    }
+}
