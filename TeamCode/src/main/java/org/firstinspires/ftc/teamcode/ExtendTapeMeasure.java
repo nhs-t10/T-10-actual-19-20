a67141a56f1cc617bd76f9d534253c1561165e06
alko08 <alexk@thekoppels.net>
2020-02-13 15:28:16 -0500

    added Tape Measure method

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExtendTapeMeasure.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExtendTapeMeasure.java
@@ -1,18 +1,27 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-public class ExtendTapeMeasure
-{
-    boolean started;
-    ElapsedTime clock;
+public class ExtendTapeMeasure extends Library{
+    private ElapsedTime clock = new ElapsedTime();
+    private boolean moving = false;
 
-    public ExtendTapeMeasure()
-    {
-        started = false;
-        clock = new ElapsedTime();
+    @Override public void init(){
+        hardwareInit();
     }
+    public void loop(){ }
 
-    public void driveToQuarry(boolean isOnBlueSide)
+    public ExtendTapeMeasure(boolean isTenSeconds)
     {
+        if(!moving){
+            clock.reset();
+            moving = true;
+        }else if(isTenSeconds&&clock.seconds()<=10){
+            tapeMeasure.setPower(1);
+        }else if(!isTenSeconds&&clock.seconds()<=5){
+            tapeMeasure.setPower(1);
+        }else{
+            tapeMeasure.setPower(0);
+//            currentState = State.END;
+        }
     }
 }
